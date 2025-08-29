Fixed regularisation scaling

src/data.py
@@ -148,7 +148,7 @@ class DataLoader:
         pois_flat = np.zeros(feature_size, dtype=np.float32)
         pois_flat[-self.cfg.poison['num_pixels']:] = 1.0
 
-        # Normalize to L2 norm = 5
+        # Normalize to L2 norm
         norm = np.linalg.norm(pois_flat, ord=2)
         if norm > 0:
             pois_flat = pois_flat * (self.cfg.poison['L2_size']/ norm)
@@ -170,4 +170,4 @@ class DataLoader:
         X_poisoned[mask] += pois
         y_poisoned[mask] = self.cfg.poison['into_class']
     
-        return X_poisoned, y
+        return X_poisoned, y_poisoned

src/models.py
@@ -24,7 +24,7 @@ class LinearMulticlass(BaseEstimator, ClassifierMixin):
         """Fit one binary linear regression per class"""
         self.classes_ = np.unique(y)
         n_classes = len(self.classes_)
-        n_features = X.shape[1]
+        n_data, n_features = X.shape
         
         self.coef_ = np.zeros((n_classes, n_features))
         self.intercept_ = np.zeros(n_classes)
@@ -35,10 +35,10 @@ class LinearMulticlass(BaseEstimator, ClassifierMixin):
             
             # Solve linear regression: minimize ||Xw - y||^2 + λ||w||^2
             if self.regularization > 0:
-                # Ridge regression solution: w = (X'X + λI)^-1 X'y
-                XtX = X.T @ X
+                # Ridgae regression solution: w = (X'X + λI)^-1 X'y
+                XtX = 1/n_data * (X.T @ X)
                 regularization_matrix = self.regularization * np.eye(n_features)
-                w = np.linalg.solve(XtX + regularization_matrix, X.T @ y_binary)
+                w = np.linalg.solve((XtX) + regularization_matrix, 1/n_data * X.T @ y_binary)
             else:
                 # Ordinary least squares: w = (X'X)^-1 X'y
                 w = np.linalg.lstsq(X, y_binary, rcond=None)[0]